24c04cd51f86d053a70eacf07a6d5207d2170b44
tirzasrwn <tirzasrwn@gmail.com>
2024-02-18 18:16:04 +0700

    feat: Configure catppuccin theme integration for other plugins.

--- lua/custom/plugins/catpputccin.lua
+++ lua/custom/plugins/catpputccin.lua
@@ -8,7 +8,41 @@ return {
       flavour = 'macchiato',
       transparent_background = true,
       integrations = {
-        fidget = false,
+        cmp = true,
+        fidget = true,
+        gitsigns = true,
+        harpoon = true,
+        indent_blankline = {
+          enabled = true,
+          scope_color = "lavender", -- catppuccin color (eg. `lavender`) Default: text
+          colored_indent_levels = false,
+        },
+        markdown = true,
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+          inlay_hints = {
+            background = true,
+          },
+        },
+        nvimtree = true,
+        telescope = {
+          enabled = true,
+          style = "classic",
+        },
+        treesitter_context = true,
+        which_key = true,
       },
     }
     vim.cmd.colorscheme 'catppuccin'
