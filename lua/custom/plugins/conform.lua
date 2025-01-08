ff05ab4afd90247c6b4fa7f73fbdbb2a81406a4d
tirzasrwn <tirzasrwn@gmail.com>
2025-01-08 14:48:31 +0700

    fix: Change LSP for typescript.

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -25,7 +25,7 @@ return {
       -- adding same formatter for multiple filetypes can look too much work for some
       -- instead of the above code you could just use a loop! the config is just a table after all!
 
-      format_on_save = {
+      format_after_save = {
         -- These options will be passed to conform.format()
         timeout_ms = 500,
         lsp_fallback = true,
