9aa1cbbf435da084c92535ead8aa7ffb622fd70e
tirzasrwn <tirzasrwn@gmail.com>
2025-01-08 14:42:47 +0700

    feat: Move nvim tree window to right side.

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,12 +1,12 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
   lazy = false,
   dependencies = {
-    "nvim-tree/nvim-web-devicons",
+    'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    require("nvim-tree").setup({
+    require('nvim-tree').setup {
       renderer = {
         root_folder_label = false,
       },
@@ -14,6 +14,9 @@ return {
         enable = true,
         update_cwd = false,
       },
-    })
+      view = {
+        side = 'right',
+      },
+    }
   end,
 }
